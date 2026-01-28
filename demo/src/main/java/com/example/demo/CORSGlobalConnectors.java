c4284900391c78c07a178a85d9525e5869aa00d3
Darshan-11-create <darshanbommineni@gmail.com>
2026-01-28 23:54:31 +0530

    Update CORSGlobalConnectors.java

--- demo/src/main/java/com/example/demo/CORSGlobalConnectors.java
+++ demo/src/main/java/com/example/demo/CORSGlobalConnectors.java
@@ -1,5 +1,4 @@
 package com.example.demo;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -8,15 +7,17 @@ import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class CORSGlobalConnectors {
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*"); // your frontend
+        
+        // Replace * with your actual frontend URL
+        config.addAllowedOrigin("https://darshan-11-create.github.io");
+        
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
-
+        
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
