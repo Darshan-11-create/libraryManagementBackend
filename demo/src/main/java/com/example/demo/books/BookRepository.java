2a6f77b2232927c0e404b4632a141ce6eb135e0c
Darshan-11-create <darshanbommineni@gmail.com>
2026-02-01 19:16:39 +0530

    Update BookRepository.java

--- demo/src/main/java/com/example/demo/books/BookRepository.java
+++ demo/src/main/java/com/example/demo/books/BookRepository.java
@@ -2,11 +2,17 @@ package com.example.demo.books;
 
 import com.example.demo.user.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import jakarta.persistence.LockModeType;
 import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book,Integer> {
 @Lock(LockModeType.PESSIMISTIC_WRITE)
 @Query("SELECT b FROM Book b WHERE b.id = :id")
-public interface BookRepository extends JpaRepository<Book,Integer> {
+  Book BuyOrRent(@Param("id") int id);
   Book findById(int id);
   List<Book> findByBookName(String name);
   List<Book>findByCustomer(Customer customer);
