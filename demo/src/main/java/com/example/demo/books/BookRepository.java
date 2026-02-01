cee8a99d52c4434ebe59224b114a9d0e4edac460
Darshan-11-create <darshanbommineni@gmail.com>
2026-02-01 19:01:35 +0530

    Update BookRepository.java

--- demo/src/main/java/com/example/demo/books/BookRepository.java
+++ demo/src/main/java/com/example/demo/books/BookRepository.java
@@ -4,7 +4,8 @@ import com.example.demo.user.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
+@Lock(LockModeType.PESSIMISTIC_WRITE)
+@Query("SELECT b FROM Book b WHERE b.id = :id")
 public interface BookRepository extends JpaRepository<Book,Integer> {
   Book findById(int id);
   List<Book> findByBookName(String name);
